bd76b93c1ab7738cbccb1b54a6b440ac3d51b91e
John Knox <jknoxiii@gmail.com>
2023-01-18 11:33:18 -0500

    Configured to allow for SD extender

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1738,7 +1738,7 @@
   #define SDCARD_CONNECTION ONBOARD
 
   // Enable if SD detect is rendered useless (e.g., by using an SD extender)
-  //#define NO_SD_DETECT
+  #define NO_SD_DETECT
 
   /**
    * Multiple volume support - EXPERIMENTAL.
